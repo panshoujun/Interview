c8888b92af1f99f57d7c1c4315ccc57b6481947b
17597 <17597@VREADY>
2020-04-23 14:58:00 +0800

    初始化mvc测试项目

---
+++ MVCTEST/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using MVCTEST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTEST.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["message"] = "这是ViewData(string)";
+
+
+            Student stu = new Student { Age = 10, Name = "潘守军", NO = "NO001" };
+            ViewBag.ViewBagStu = stu;
+            ViewData["ViewDataStu"] = stu;
+            TempData["TempDataStu"] = stu;
+
+            return View();
+        }
+
+
+        public ActionResult Index2()
+        {
+            Student stu = new Student { Age = 10, Name = "潘守军", NO = "NO001" };
+            ViewBag.ViewBagStu = stu;
+            ViewData["ViewDataStu"] = stu;
+            TempData["TempDataStu"] = stu;
+
+            return RedirectToAction("index3");
+        }
+
+        public ActionResult Index3()
+        {
+            var stu=ViewBag.ViewBagStu;
+            var stu2 = ViewData["ViewDataStu"];
+            var stu3 = TempData["TempDataStu"];
+            return RedirectToAction("About");
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            string message = ViewData["message"] as string;
+            var stu = ViewBag.ViewBagStu;
+            var stu2 = ViewData["ViewDataStu"];
+            var stu3 = TempData["TempDataStu"];
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCTEST/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTEST.Models
+{
+    /// <summary>
+    /// 学生
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string NO { get; set; }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"我的名字叫{Name},今年{Age}岁了;序号是{NO}";
+        }
+
+    }
+}
