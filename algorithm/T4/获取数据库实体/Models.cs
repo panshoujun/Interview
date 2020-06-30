8ae829f5de39076f93d0de072af91c5901a0ccfe
panshoujun <panshoujun@SC-PANSHOUJUN>
2020-06-30 15:10:32 +0800

    实体生成

---
+++ algorithm/T4/获取数据库实体/Models.cs
@@ -0,0 +1,91 @@
+﻿using System;
+namespace MyProject.Entities
+{     
+        
+           public class ChargeStationController                   
+           {      
+                /// <summary>
+                /// 
+                /// </summary>
+                 public String  ID { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public String  Code { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public String  ControlAddress { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public String  StationID { get; set; }                                                                                           
+                       
+           }                  
+             
+           public class TestCol                   
+           {      
+                /// <summary>
+                /// 
+                /// </summary>
+                 public Int64?  ID { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 姓名
+                /// </summary>
+                 public String  Name { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public DateTime?  DateTime { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public Byte[]  Image { get; set; }                                                                                           
+                       
+           }                  
+             
+           public class __EFMigrationsHistory                   
+           {      
+                /// <summary>
+                /// 
+                /// </summary>
+                 public String  MigrationId { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public String  ProductVersion { get; set; }                                                                                           
+                       
+           }                  
+             
+           public class ChargeStation                   
+           {      
+                /// <summary>
+                /// 主键ID
+                /// </summary>
+                 public String  ID { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 编码
+                /// </summary>
+                 public String  Code { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 名称
+                /// </summary>
+                 public String  Name { get; set; }                                                                                           
+                
+                /// <summary>
+                /// 
+                /// </summary>
+                 public Int64  MaintainTel { get; set; }                                                                                           
+                       
+           }                  
+                           
+}
